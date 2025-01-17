ef27530ed2a02f5e743c7a5d29554069c0ea61c5
BeBrandao <beabs2305@gmail.com>
2025-01-17 10:27:30 -0300

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -23,12 +23,12 @@ const flappyBird = {
 }
 
 const cenario = {
-    spriteX: 0,
-    spriteY: 0,
-    largura: 320,
-    altura: 206,
+    spriteX: 390,
+    spriteY: 3,
+    largura: 277,
+    altura: 203,
     x: 0,
-    y: 274,
+    y: 280  ,
     desenha() {
         contexto.drawImage(
             sprites,
@@ -37,12 +37,47 @@ const cenario = {
             cenario.x, cenario.y,
             cenario.largura, cenario.altura,
         );
+        contexto.drawImage(
+            sprites,
+            cenario.spriteX, cenario.spriteY,
+            cenario.largura, cenario.altura,
+            cenario.x + 276, cenario.y,
+            cenario.largura, cenario.altura,
+        );
     }
 }
 
+const chao = {
+    spriteX: 0,
+    spriteY: 608,
+    largura: 224,
+    altura: 113,
+    x: 0,
+    y: 367,
+    desenha() {
+        contexto.drawImage(
+            sprites,
+            chao.spriteX, chao.spriteY,
+            chao.largura, chao.altura,
+            chao.x, chao.y,
+            chao.largura, chao.altura,
+        );
+        contexto.drawImage(
+            sprites,
+            chao.spriteX, chao.spriteY,
+            chao.largura, chao.altura,
+            chao.x + 224, chao.y,
+            chao.largura, chao.altura,
+        );
+    }
+}
+
+contexto.fillStyle = '#70c5ce';
+contexto.fillRect(0,0, canvas.width, canvas.height)
 function loop(){
     flappyBird.desenha();
     cenario.desenha();
+    chao.desenha();
 
     requestAnimationFrame(loop);
 }
