f3b170f29cda277d5b1e1d52c6bb5dea6258a18d
BeBrandao <beabs2305@gmail.com>
2025-01-19 17:36:50 -0300

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -5,13 +5,21 @@ const canvas = document.querySelector('#game-canvas');
 const contexto = canvas.getContext('2d');
 
 const flappyBird = {
+
     spriteX: 0,
     spriteY: 0,
     largura: 35,
     altura: 25,
     x: 10,
     y: 50,
+    gravidade: 0.25,
+    velocidade: 0,
+    limiteSuperior: 0,
+    limiteInferior: 367,
+    contadorFrame: 0,
+    
     desenha() {
+
         contexto.drawImage(
             sprites,
             flappyBird.spriteX, flappyBird.spriteY,
@@ -19,17 +27,58 @@ const flappyBird = {
             flappyBird.x, flappyBird.y,
             flappyBird.largura, flappyBird.altura,
         );
+    },
+
+    atualiza(){
+        flappyBird.velocidade += flappyBird.gravidade;
+        flappyBird.y += flappyBird.velocidade;
+
+        flappyBird.contadorFrame++;
+        if (flappyBird.contadorFrame % 10 === 0) {
+            if (flappyBird.spriteY === 0 ) {
+                flappyBird.spriteY = 25;
+            } else if (flappyBird.spriteY === 25) {
+                flappyBird.spriteY = 50;
+            } else {
+                flappyBird.spriteY = 0;
+            }
+        }
+
+
+        if (flappyBird.y < flappyBird.limiteSuperior) {
+            flappyBird.y = flappyBird.limiteSuperior;
+            flappyBird.velocidade = 0
+        }
+        
+        const limiteInferior = canvas.height - chao.altura;
+        if (flappyBird.y + flappyBird.altura >= limiteInferior){
+            flappyBird.y = limiteInferior - flappyBird.altura;
+            reiniciaJogo();
+        }
+    },
+
+    sobe(){
+        flappyBird.velocidade = -5;
     }
+};
+
+function reiniciaJogo() {
+    flappyBird.y = 50;
+    flappyBird.velocidade = 0;
+    telaAtiva = TelaInicio;
 }
 
 const cenario = {
+
     spriteX: 390,
     spriteY: 3,
     largura: 277,
     altura: 203,
     x: 0,
-    y: 280  ,
+    y: 280,
+
     desenha() {
+
         contexto.drawImage(
             sprites,
             cenario.spriteX, cenario.spriteY,
@@ -48,12 +97,14 @@ const cenario = {
 }
 
 const chao = {
+
     spriteX: 0,
     spriteY: 608,
     largura: 224,
     altura: 113,
     x: 0,
     y: 367,
+
     desenha() {
         contexto.drawImage(
             sprites,
@@ -72,13 +123,67 @@ const chao = {
     }
 }
 
-contexto.fillStyle = '#70c5ce';
-contexto.fillRect(0,0, canvas.width, canvas.height)
-function loop(){
-    flappyBird.desenha();
-    cenario.desenha();
-    chao.desenha();
+const inicio = {
 
+    spriteX: 130,
+    spriteY: 0,
+    largura: 180,
+    altura: 152,
+    x: 70,
+    y: 70,
+
+    desenha() {
+        contexto.drawImage(
+            sprites,
+            inicio.spriteX, inicio.spriteY,
+            inicio.largura, inicio.altura,
+            inicio.x, inicio.y,
+            inicio.largura, inicio.altura, 
+        );
+    }
+}
+
+const TelaInicio = {
+
+    desenha(){
+        cenario.desenha();
+        chao.desenha();
+        flappyBird.desenha();
+        inicio.desenha();
+    },
+
+    click(){
+        telaAtiva = TelaJogo;
+    }
+}
+
+const TelaJogo = {
+
+    desenha(){
+        cenario.desenha();
+        chao.desenha();
+        flappyBird.desenha();
+        flappyBird.atualiza();
+    },
+
+    click(){
+        flappyBird.sobe();
+    }
+}
+
+var telaAtiva = TelaInicio;
+
+function mudaTelaAtiva(){
+    telaAtiva.click();
+}
+
+window.addEventListener("click", mudaTelaAtiva);
+
+
+function loop(){
+    contexto.fillStyle = '#70c5ce';
+    contexto.fillRect(0,0, canvas.width, canvas.height)
+    telaAtiva.desenha()
     requestAnimationFrame(loop);
 }
 
